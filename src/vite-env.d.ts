2886af7717ac54ce0e9b4100505d9fdaedca6570
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-09-17 20:15:00 +0530

    add mock server, tailwind and other config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly NODE_ENV: string
+    readonly VITE_API_URL: string
+    // more env variables...
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
